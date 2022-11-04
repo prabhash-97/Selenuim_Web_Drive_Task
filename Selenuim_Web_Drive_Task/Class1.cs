c2184dfe9a1189809181ba01408f172699da8538
Kasuni <kasunirashmika568@gmail.com>
2022-11-04 08:55:44 +0530

    add changes

--- Selenuim_Web_Drive_Task/Class1.cs
+++ Selenuim_Web_Drive_Task/Class1.cs
@@ -2,14 +2,7 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using OpenQA.Selenium.Support.UI;
-using System.Threading;
-using System.IO;
 
 namespace Selenuim_Web_Drive_Task
 {
@@ -26,24 +19,26 @@ namespace Selenuim_Web_Drive_Task
         }
 
         [Test]
-        public void test()
+        public void Test()
         {
 
             m_driver.Url = testURL;
             m_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
 
             IWebElement course = m_driver.FindElement(By.XPath(".//*[@id='awf_field-91977689']"));
-            var selectTest = new SelectElement(course);
-            selectTest.SelectByValue("sap-abap");
-
-            m_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-
             IWebElement emailTextBox = m_driver.FindElement(By.CssSelector("input[id=philadelphia-field-email]"));
-            emailTextBox.Clear();
-            emailTextBox.SendKeys("test123@gmail.com");
 
             WebDriverWait wait = new WebDriverWait(m_driver, TimeSpan.FromSeconds(50));
             IWebElement signUpButton = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(".//*[@id='philadelphia-field-submit']")));
+
+            var selectTest = new SelectElement(course);
+            selectTest.SelectByValue("sap-abap");
+
+            m_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+            emailTextBox.Clear();
+            emailTextBox.SendKeys("test123@gmail.com");
+    
             signUpButton.Click();
 
             var expectedAlertText = "Form Submitted Successfully...";
